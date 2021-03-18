aa01969f3c4b38dc26f668559785809a87b07873
César de la Cal Bretschneider <cesar@magic3w.com>
2021-03-18 10:45:58 +0100

    Migrated director to sf 0.2
    Test Plan: The director should be compatible with SF 0.2
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D598

--- src/directors/ProcessDirector.php
+++ src/directors/ProcessDirector.php
@@ -1,35 +1,53 @@
-<?php namespace spitfire\defer;
+<?php namespace spitfire\defer\directors;
 
 use spitfire\cli\Console;
+use spitfire\defer\Task;
 use spitfire\mvc\Director;
+use spitfire\provider\Container;
 use Throwable;
-use function console;
-use function db;
 
-class AsyncDirector extends Director
+class ProcessDirector extends Director
 {
 	
 	private $db;
 	private $console;
+	private $services;
 	
-	public function __construct(\spitfire\storage\database\DB $db, Console $console)
+	public function __construct(\spitfire\storage\database\DB $db, Console $console, Container $services)
 	{
 		$this->db = $db;
 		$this->console = $console;
+		$this->services = $services;
 	}
 	
-	public function pending() {
-		
+	/**
+	 * The process function does not yet receive any parameters.
+	 */
+	public function parameters() {
+		return [];
 	}
 	
-	public function process() {
+	public function body() {
 		$pending = $this->db->table('spitfire\core\async\Async')->get('status', 'pending')
 			->where('scheduled', '<', time())
 			->all();
 		
 		foreach ($pending as $record) {
-			/*@var $task Task*/
-			$task = unserialize($record->task);
+			/*@var $task \spitfire\defer\Task*/
+			$task = $this->services->get($record->task);
+			
+			/**
+			 * If a task is not a task that we can execute, we need to not execute it since it may
+			 * cause behavior that we did not anticipate.
+			 */
+			if (!($task instanceof Task)) { 
+				$record->status = 'aborted';
+				$record->store();
+				$this->console->error('Task is not a valid Task. Cannot process this task')->ln();
+				continue;
+			}
+			
+			$task->setSettings(json_decode($task->setttings));
 			
 			if ($record->ttl < 1) {
 				$record->status = 'aborted';
