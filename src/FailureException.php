eb88b65a350b4e8d78745c2bdce03d1dd238a3e1
César de la Cal Bretschneider <cesar@magic3w.com>
2021-03-31 15:37:39 +0200

    Added phpstan
    Test Plan:
    `composer test`
    
    This is not error free. Since there's a bunch of files actively being changed by
    other diffs now.
    
    Reviewers: cesar
    
    Reviewed By: cesar
    
    Differential Revision: https://phabricator.magic3w.com/D657

--- src/FailureException.php
+++ src/FailureException.php
@@ -5,15 +5,28 @@ use Exception;
 class FailureException extends Exception
 {
 	
+	/**
+	 * @var string
+	 */
 	private $extended;
 	
+	/**
+	 * 
+	 * @param string $message
+	 * @param int $code
+	 * @param string $extended
+	 */
 	public function __construct(string $message = "", int $code = 0, string $extended = "") {
 		$this->extended = $extended;
 		parent::__construct($message, $code, null);
 	}
 
-	
-	public function getExtended() {
+	/**
+	 * 
+	 * @return string
+	 */
+	public function getExtended() : string
+	{
 		return $this->extended;
 	}
 
--- src/Result.php
+++ src/Result.php
@@ -3,12 +3,31 @@
 class Result
 {
 	
+	/**
+	 * This is the message that will be written to the log as the result of the
+	 * task being executed. This is expected to be a human readable string
+	 * 
+	 * @var string
+	 */
 	private $payload;
 	
+	
+	/**
+	 * The result of the operation. This contains a message that will be recorded 
+	 * for the maintainer to understand what the task did.
+	 * 
+	 * @param string $payload
+	 */
 	public function __construct($payload) {
 		$this->payload = $payload;
 	}
 	
+	/**
+	 * Returns the message that will be written to the log as the result of the
+	 * task being executed. This is expected to be a human readable string
+	 * 
+	 * @return string
+	 */
 	public function getPayload() {
 		return $this->payload;
 	}
